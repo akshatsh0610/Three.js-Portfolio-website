caff2332fdfa6e350e18d7e7b712176f6dcf7c99
Akshat <akshatsharma0610@gmail.com>
2023-05-06 12:05:39 +0530

    Commit1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,31 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi I'm <span className="text-[#915eff]">Akshat</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Neque porro quisquam est qui dolorem <br/> ipsum quia dolor sit amet, consectetur,<br/> adipisci velit.
+            Neque porro quisquam est qui dolorem <br/> ipsum quia dolor sit amet, consectetur
           </p>
         </div>
       </div>
       <ComputersCanvas/>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div 
+            className="w-[35px] h-[64px] rounded-3xl border-4
+            border-secondary flex justify-center items-start p-2" 
+          >
+            <motion.div
+              animate={{
+                y:[0,24,0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,21 @@
-import { Html } from '@react-three/drei'
+import { Html,useProgress } from '@react-three/drei'
 // import React from 'react'
 // import { Canvas } from '@react-three/fiber'
 const Loader = () => {
+  const {progress}=useProgress();
   return (
-   <Html></Html>
+   <Html>
+    <span className='canvas-load'>
+      <p
+        style={{
+          fontSize : 14,
+          color : '#f1f1f1',
+          fontWeight : 800,
+          marginTop : 40
+        }}  
+      >{progress.toFixed(2)}%</p>
+    </span>
+   </Html>
   )
 }
 
